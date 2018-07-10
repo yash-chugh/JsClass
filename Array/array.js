e5ea77aef941deca450ce123dc80622328a6fb42
_grvsharma <gaurav.sharma@zenways.io>
2018-07-10 15:48:48 +0530

    Array

---
+++ Array/array.js
@@ -0,0 +1,124 @@
+
+// let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let arrObj = [
+//     {
+//         name : 'abc'
+//     },
+//     {
+//         name : 'name'
+//     }
+// ];
+
+// console.log(fruits[0]);
+
+// console.log(arrObj[1].name);
+
+// console.log(arrNum.length);
+
+
+// console.log(arrNum.indexOf(10));
+
+// let arrNum = [1,2,3,4,5,6,7];
+
+// let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(fruits[fruits.length - 1]);
+
+
+// fruits.push('kiwi')
+
+// console.log(fruits);
+
+// fruits.pop();
+
+// console.log(fruits);
+
+
+// fruits.unshift('kiwi');
+
+// console.log(fruits)
+
+
+// fruits.shift();
+
+// console.log(fruits);
+
+
+// fruits.splice(2, 0, 'Kiwi');
+
+// console.log(fruits)
+
+// fruits.splice(2,2);
+
+// console.log(fruits)
+
+// fruits.splice(2,1,'kiwi');
+
+// console.log(fruits);
+
+// console.log(fruits.join(' - '));
+
+// let arrNum = [1,2,3,4,5,6,7];
+
+// console.log(fruits.concat(arrNum));
+
+// console.log(fruits.slice(2));
+
+// console.log(fruits.slice(1,3));
+
+// let arr = fruits.reverse();
+
+// console.log(arr);
+
+
+// let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let result = fruits.filter(
+//     (item , index , arr) => {
+//         // console.log(item, index,  arr);
+
+//         return item.length > 5
+//     }
+// )
+
+// let result = fruits.filter(item => item.length > 5)
+
+// console.log(result)
+
+// let arrNum = [1,2,3,4,5,6,7,8,9,10];
+
+// let result = arrNum.map(item => item*2)
+
+// console.log(result);
+
+
+// let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+
+// let arrNum = [1,2,3,4,5,6,7,8,9,10];
+
+// let result = arrNum.reduce(
+//     (acc, current , index, arr) => {
+//         console.log(acc, current , index, arr);
+
+//         return acc + current
+//     }
+// )
+
+// let result = arrNum.reduce((acc, curr) => acc + curr);
+
+
+// let onLiner = (acc, curr) => acc + curr
+
+// let result = arrNum.reduce(onLiner, 10);
+
+
+// console.log(result)
+
+
+// let arrNum = [1,2,3,4,5,6,7,8,9,10];
+
+// let result = arrNum.every( ele => ele >= 1 );
+
+// console.log(result);
